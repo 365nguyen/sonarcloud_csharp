35aa75e0ddaaf1b595660c06cab33ccd2ab90c09
Minh Tuyen Nguyen <mnguyen@congressionalfcu.org>
2024-06-26 14:41:38 -0400

    save

--- ClassLibrary/AccountRespository.cs
+++ ClassLibrary/AccountRespository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ClassLibrary
             return a + b;
         }
 
+        [ExcludeFromCodeCoverage]
         public int Subtract(int a, int b)
         {
             return a - b;
