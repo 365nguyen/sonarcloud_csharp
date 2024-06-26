a40d1734bef1fa4bcbbc39cddf009534434b504f
Minh Tuyen Nguyen <mnguyen@congressionalfcu.org>
2024-06-26 13:18:55 -0400

    Save.

---
+++ ClassLibrary.Tests/AccountRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass]
+    public class AccountRepositoryTest:AccountRespository
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(1==1);
+        }
+    }
+}
